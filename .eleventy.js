054ca6d98636e561ca7d4348c3d4618a98ec5c3c
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2025-02-11 01:32:36 -0500

    init

---
+++ .eleventy.js
@@ -0,0 +1,6 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.addPassthroughCopy("ico")
+    eleventyConfig.addPassthroughCopy("asset/img")
+    eleventyConfig.addPassthroughCopy("asset/art")
+};
