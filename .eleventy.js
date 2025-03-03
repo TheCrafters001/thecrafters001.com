863a2309ecfe37d304b4938cfbc383a59ace9a6f
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2025-03-03 02:46:40 -0500

    Changed to Minify HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,28 @@
+// For Minify
+const htmlmin = require("html-minifier-terser");
+
+// Config
 module.exports = function(eleventyConfig) {
+    // Passthrough
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.addPassthroughCopy("ico")
     eleventyConfig.addPassthroughCopy("asset/img")
     eleventyConfig.addPassthroughCopy("asset/art")
     eleventyConfig.addPassthroughCopy("CNAME")
+
+    // Minify
+    eleventyConfig.addTransform("htmlmin", function (content) {
+		if ((this.page.outputPath || "").endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+			});
+
+			return minified;
+		}
+
+		// If not an HTML output, return content as-is
+		return content;
+	});
 };
