9b586b1f7b21a47d94b726c98968fe0f7ef7126e
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2025-06-29 18:11:16 -0400

    Added RSS Feed

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ const htmlmin = require("html-minifier-terser");
 // luxon
 const { DateTime } = require("luxon");
 
+// RSS
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
+
 // Config
 module.exports = function(eleventyConfig) {
     // Passthrough
@@ -35,5 +38,26 @@ module.exports = function(eleventyConfig) {
 		return DateTime.fromJSDate(dateObj, {
 			zone: "America/New_York",
 		}).setLocale('en').toLocaleString(DateTime.DATETIME_FULL);
+	});
+
+
+	// RSS
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/notes/feed.xml",
+		collection: {
+			name: "post", // iterate over `collections.posts`
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en-US",
+			title: "Notes",
+			subtitle: "Just a bunch of ramblings or blog posts.",
+			base: "https://TheCrafters001.com/",
+			author: {
+				name: "TheCrafters001",
+				email: "", // Optional
+			}
+		}
 	});
 };
